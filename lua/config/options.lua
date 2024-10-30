8a115be43566f8f5ff4441ba542e658b3bf8a0bd
Paweł Mstowski <pawel.mstowski@gmail.com>
2024-10-30 17:58:44 +0100

    add cloroscheme and setup basic bindings for telescope

---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+local opt = vim.opt
+-- Set all global vim options below
+
+opt.number = true
+opt.relativenumber = true
+opt.colorcolumn = "120"
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.spelllang = {"en", "pl"}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,12 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
+	keys = function()
+		local telescope = require('telescope.builtin')
+		local theme = require("telescope.themes").get_dropdown({})
+		return {
+			{"<leader>ff", function() telescope.find_files(theme) end, desc = "Find Files"},
+			{"<leader>f/", function() telescope.live_grep(theme) end, desc = "Live Grep Files"},
+		}
+	end,
 }
