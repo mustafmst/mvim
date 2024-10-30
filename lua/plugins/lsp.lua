7d8bda98225ef17542a91599b935465a155d3475
Paweł Mstowski <pawel.mstowski@gmail.com>
2024-10-30 20:41:56 +0100

    basic lsp setup for lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,72 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require('mason').setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require('lspconfig')
+			require'lspconfig'.lua_ls.setup {
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT'
+						},
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME
+								-- Depending on the usage, you might want to add additional paths here.
+								-- "${3rd}/luv/library"
+								-- "${3rd}/busted/library",
+							}
+							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+							-- library = vim.api.nvim_get_runtime_file("", true)
+						}
+					})
+				end,
+				settings = {
+					Lua = {}
+				}
+			}
+		end,
+		keys = {
+			{ "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
+      { "gd", vim.lsp.buf.definition, desc = "Goto Definition"},
+      { "gr", vim.lsp.buf.references, desc = "References", nowait = true },
+      { "gI", vim.lsp.buf.implementation, desc = "Goto Implementation" },
+      { "gy", vim.lsp.buf.type_definition, desc = "Goto T[y]pe Definition" },
+      { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
+      { "K", vim.lsp.buf.hover, desc = "Hover" },
+      { "gK", vim.lsp.buf.signature_help, desc = "Signature Help"},
+      { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help"},
+      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }},
+      { "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "v" }},
+      { "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" }},
+      { "<leader>cr", vim.lsp.buf.rename, desc = "Rename"},
+		},
+	},
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,49 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-	keys = function()
-		local telescope = require('telescope.builtin')
-		local theme = require("telescope.themes").get_dropdown({})
-		return {
-			{"<leader>ff", function() telescope.find_files(theme) end, desc = "Find Files"},
-			{"<leader>f/", function() telescope.live_grep(theme) end, desc = "Live Grep Files"},
-			{"<leader>fb", function() telescope.buffers(theme) end, desc = "Open Buffers"},
-			-- TODO: set select string grep to normal and visual mode
-			{"<leader>f*", function() telescope.grep_string(theme) end, desc = "Live Grep Selected String"},
-		}
-	end,
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		keys = function()
+			local telescope = require('telescope.builtin')
+			local theme = require("telescope.themes").get_dropdown({})
+			return {
+				{"<leader>ff", function() telescope.find_files(theme) end, desc = "Find Files"},
+				{"<leader>f/", function() telescope.live_grep(theme) end, desc = "Live Grep Files"},
+				{"<leader>fb", function() telescope.buffers(theme) end, desc = "Open Buffers"},
+				-- TODO: set select string grep to normal and visual mode
+				{"<leader>f*", function() telescope.grep_string(theme) end, desc = "Live Grep Selected String"},
+			}
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			-- This is your opts table
+			require("telescope").setup {
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {
+							-- even more opts
+						}
+
+						-- pseudo code / specification for writing custom displays, like the one
+						-- for "codeactions"
+						-- specific_opts = {
+						--   [kind] = {
+						--     make_indexed = function(items) -> indexed_items, width,
+						--     make_displayer = function(widths) -> displayer
+						--     make_display = function(displayer) -> function(e)
+						--     make_ordinal = function(e) -> string
+						--   },
+						--   -- for example to disable the custom builtin "codeactions" display
+						--      do the following
+						--   codeactions = false,
+						-- }
+					}
+				}
+			}
+			-- To get ui-select loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
