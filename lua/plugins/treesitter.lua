51cf3c700b956afc9e4b9ceb326ae978ec5793b8
Paweł Mstowski <pawel.mstowski@gmail.com>
2024-10-30 18:11:16 +0100

    add tresitter | basic config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	opt = {
+		ensure_installed = {"lua",},
+		highlight = {
+			enable = true,
+		},
+		sync_install = false,
+		indent = { enable = true, },
+		auto_install = true,
+	},
+}
